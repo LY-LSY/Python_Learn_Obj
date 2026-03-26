f7ed8ca90d88cff343827ff4ba857e26cf970eb6
LY-LSY <liuyuezhifeng@126.com>
2026-03-27 00:16:05 +0800

    unbuntu 提交

--- data_visualization‌/mpl_squares.py
+++ data_visualization‌/mpl_squares.py
@@ -4,4 +4,13 @@ squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 fig, ax = plt.subplots()
 ax.plot(squares)
+
+# 设置图题并给坐标加上标签
+ax.set_title('Square Numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+# 设置刻度标记的样式
+ax.tick_params(labelsize=14)
+
 plt.show()
