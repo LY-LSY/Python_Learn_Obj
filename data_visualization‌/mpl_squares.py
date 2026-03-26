6fa0d02863267378fee6863fdea392d82d6f532c
LY-LSY <liuyuezhifeng@126.com>
2026-03-27 00:16:05 +0800

    【项目名称】Pyton项目入门练习 【提交说明】数据可视化：散点图，色标设置 【提交备注】NA

--- data_visualization‌/mpl_squares.py
+++ data_visualization‌/mpl_squares.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
+plt.style.use('seaborn-v0_8-bright')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+# 设置图题并给坐标加上标签
+ax.set_title('Square Numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+# 设置刻度标记的样式
+ax.tick_params(labelsize=14)
+
 plt.show()
