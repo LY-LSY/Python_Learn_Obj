140c104391ace99047e216040a6e2aef02a598cc
LY-LSY <liuyuezhifeng@126.com>
2026-06-17 23:22:03 +0800

    【项目名称】Python项目入门练习 【提交说明】制作全球地震散点图：plotly.express输出html

---
+++ data_visualization‌/eq_explore_data.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import json
+import plotly.express as px
+
+# 将数据作为字符串读取并转换为python对象
+path = Path('eq_data/eq_data_1_day_m1.json')
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+# # 将数据文件转换为更易于阅读的版本
+# path = Path('eq_data/readable_eq_data.json')
+# readable_contents = json.dumps(all_eq_data, indent=4)
+# path.write_text(readable_contents)
+
+# 查看数据集中的所有地震
+all_eq_dicts = all_eq_data['features']
+
+mags, titles, lons, lats = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
+
+fig = px.scatter(
+    x=lons,
+    y=lats,
+    labels={'x': '经度', 'y': '维度'},
+    range_x=[-200, 200],
+    range_y=[-90, 90],
+    width=800,
+    height=800,
+    title='全球地震散点图'
+)
+fig.write_html('global_earthquakes.html')
+fig.show()
